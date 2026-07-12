0901fc8e48888491da9a7ffdf64d1f2f50dab873
SOUVIK <souvikjana85794@gmail.com>
2026-07-12 10:53:30 +0530

    v1.0-alpha Added local player camera and smooth camera interpolation

--- client/camera.js
+++ client/camera.js
@@ -5,17 +5,20 @@ export let cameraY = 0;
 
 export function updateCamera(players) {
 
-    //const ids = Object.keys(players);
-
-    
     const p = players[myId];
 
-    if(!p) return;
+    if (!p) return;
 
-    cameraX =
+    const targetX =
         p.x - window.innerWidth / 2 + 25;
 
-    cameraY =
+    const targetY =
         p.y - window.innerHeight / 2 + 25;
+
+    cameraX +=
+        (targetX - cameraX) * 0.10;
+
+    cameraY +=
+        (targetY - cameraY) * 0.10;
 
 }
